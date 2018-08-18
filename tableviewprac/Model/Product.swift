e0f7bb6a7755f04b52b3eeb67f46c882546f9ddf
Admin <admin@Admins-iMac.local>
2018-08-18 23:14:34 +0500

    done

---
+++ tableviewprac/Model/Product.swift
@@ -0,0 +1,9 @@
+//
+//  Product.swift
+//  tableviewprac
+//
+//  Created by Admin on 18/08/2018.
+//  Copyright © 2018 abdulahad. All rights reserved.
+//
+
+import Foundation
---
+++ tableviewprac/View/ProductCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCell.swift
+//  tableviewprac
+//
+//  Created by Admin on 18/08/2018.
+//  Copyright © 2018 abdulahad. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+}
